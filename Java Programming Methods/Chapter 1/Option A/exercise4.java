4033a07a1423e09816a5f50739dd38be78f5f79e
niksirotkin98 <57777796+niksirotkin98@users.noreply.github.com>
2020-06-13 03:01:24 +0300

    Add first chapter solution's
    But i don't comment them. Do this later. Perhaps.

---
+++ Java Programming Methods/Chapter 1/Option A/exercise4.java
@@ -0,0 +1,18 @@
+package optionA;
+
+import java.util.Scanner;
+
+public class task4 {
+
+	public static void main(String[] args) {
+		final String password = "password";
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter password:");
+		if(sc.next().indexOf(password) != -1)
+			System.out.print("Valid password");
+		else
+			System.out.print("Invalid password");
+		sc.close();
+	}
+
+}
---
+++ Java Programming Methods/Chapter 1/Option A/exercise5.java
@@ -0,0 +1,22 @@
+package optionA;
+
+public class task5 {
+
+	public static void main(String[] args) {
+		if(args.length == 0) {
+			System.out.println("No arguments passed");
+			return;
+		}
+		try {
+			long sum=0, mul=1;
+			for(String s : args) {
+				int i = Integer.valueOf(s);
+				sum += i; mul *= i;
+			}
+			System.out.println("Argument's sum:"+sum+"\nArgument's multiplication:"+mul);
+		} catch(Exception e) {
+			System.out.println("Invalid argument: "+e);
+		}
+	}
+
+}
